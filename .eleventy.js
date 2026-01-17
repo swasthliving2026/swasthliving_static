7804404547d18ee7fadd5cab23f54c58fc1cba9c
swasthliving2026 <support@swasthliving.in>
2026-01-17 10:52:53 +0530

    Update .eleventy.js
    added all other static pages to eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,15 @@ export default function (eleventyConfig) {
   eleventyConfig.addLayoutAlias("base", "base.njk");
   eleventyConfig.addLayoutAlias("blog", "blog.njk");
   eleventyConfig.addPassthroughCopy("assets");
+  // Homepage
   eleventyConfig.addPassthroughCopy("index.html");
+
+  // Static pages
+  eleventyConfig.addPassthroughCopy("contact");
+  eleventyConfig.addPassthroughCopy("programs");
+  eleventyConfig.addPassthroughCopy("privacy-policy");
+  eleventyConfig.addPassthroughCopy("terms-and-conditions");
+
   
   eleventyConfig.addCollection("blog", function (collectionApi) {
     return collectionApi.getFilteredByGlob("src/blog/**/*.md");
